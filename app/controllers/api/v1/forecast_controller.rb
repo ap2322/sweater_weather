48ccf8aea09d088b9612bb56818df2a0983dd088
Alice Post <alicep2322@gmail.com>
2020-01-12 15:38:10 -0700

    Update controller to use the GoogleGeoService

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::ForecastController < ApplicationController
 
   def index
+    google_service = GoogleGeoService.new(params['location'])
+    lat_long = google_service.coordnates
+    location_info = google_service.city_state_country
+
+    binding.pry
   end
-  
+
 end
