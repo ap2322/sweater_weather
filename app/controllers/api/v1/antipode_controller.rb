f5ad30b9a02ca5571335184bce207dee4dfaaf69
Alice Post <alicep2322@gmail.com>
2020-01-13 10:33:22 -0700

    Use antipode facade in controller

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,23 +1,12 @@
 class Api::V1::AntipodeController < ApplicationController
 
   def show
-    # get lat and long of search
-    google_service = GoogleGeoService.new(params[:location])
-    search_lat_long = google_service.coordinates
-
-    # send lat and long of search to amy pode api for antipode
-    antipode_lat_long = AmypodeService.antipode(search_lat_long)
-
-    # send antipode lat and long to google geocode api for city name
-    antipode_location_name = google_service.find_place(antipode_lat_long)
-
-    # send antipode lat and long to darksky api for current weather summary and temp
-    darksky_service = DarkskyService.new(antipode_lat_long)
-    antipode_forecast = darksky_service.forecast
-
-    # Serialize antipode forecast
+    antipode_info = AntipodeFacade.new(params[:location])
     hash_output = AntipodeForecastSerializer.serializable_hash(
-      params[:location], antipode_location_name, antipode_forecast)
+      params[:location],
+      antipode_info.antipode_location_name,
+      antipode_info.antipode_forecast)
+
     render json: hash_output.to_json
   end
 
