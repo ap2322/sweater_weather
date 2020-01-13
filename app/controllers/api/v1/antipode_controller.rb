1e91b2e8da24e0fb027f60d3dcc96a5d3446c0f0
Alice Post <alicep2322@gmail.com>
2020-01-13 10:04:45 -0700

    Continuing to dream drive antipode show action

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -6,9 +6,10 @@ class Api::V1::AntipodeController < ApplicationController
     search_lat_long = google_service.coordinates
 
     # send lat and long of search to amy pode api for antipode
-    
+    antipode_lat_long = AmypodeService.antipode(search_lat_long)
 
     # send antipode lat and long to google geocode api for city name
+    antipode_location_name = google_service.find_place(antipode_lat_long)
 
     # send antipode lat and long to darksky api for current weather summary and temp
   end
