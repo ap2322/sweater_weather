73a6cedbf710eda715e4ccd9c78e6c566f614157
Alice Post <alicep2322@gmail.com>
2020-01-13 11:38:12 -0700

    Keep forecast returned from coordinates

--- app/facades/antipode_facade.rb
+++ app/facades/antipode_facade.rb
@@ -26,18 +26,8 @@ class AntipodeFacade
   end
 
   def get_antipode_forecast
-    full_forecast = {}
-    if antipode_location_name == "Sorry, no places found for those coordinates"
-      return full_forecast = {
-        currently: {
-          summary: 'No location for forecast',
-          temperature: 'No location for forecast',
-        }
-      }
-    else
-      darksky_service = DarkskyService.new(antipode_coordinates)
-      darksky_service.forecast
-    end
+    darksky_service = DarkskyService.new(antipode_coordinates)
+    darksky_service.forecast
   end
 
   private
