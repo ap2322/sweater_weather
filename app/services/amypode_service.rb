1f55ecafb7f863f70efa95e71848e6bdf61a3c02
Alice Post <alicep2322@gmail.com>
2020-01-13 09:42:24 -0700

    Add AmypodeService with antipode method

---
+++ app/services/amypode_service.rb
@@ -0,0 +1,18 @@
+class AmypodeService
+
+  def self.antipode(lat_long)
+    ll_array = lat_long.split(',')
+    lat = ll_array[0]
+    long = ll_array[1]
+    response = Faraday.get('http://amypode.herokuapp.com/api/v1/antipodes') do |req|
+      req.params['lat'] = lat
+      req.params['long'] = long
+      req.headers['api_key'] = 'oscar_the_grouch'
+    end
+    json_response = JSON.parse(response.body, symbolize_names: true)
+    anti_lat = json_response[:data][:attributes][:lat]
+    anti_long = json_response[:data][:attributes][:long]
+    "#{anti_lat},#{anti_long}"
+  end
+  
+end
