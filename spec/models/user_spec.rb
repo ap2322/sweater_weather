0e7f4f484496b8207ae22871dec1215068eac72c
Alice Post <alicep2322@gmail.com>
2020-01-12 22:48:35 -0700

    Add before create and test for adding api_key to users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,15 @@ describe User, type: :model do
     it {should validate_presence_of :email}
     it {should validate_uniqueness_of :email}
   end
+
+  describe 'methods' do
+    it 'adds an api key for a user before create' do
+      user_count = User.all.count
+      user = User.create(email: 'example@email.com', password: 'password', password_confirmation: 'password')
+
+      new_user_count = User.all.count
+      expect(user.api_key).to_not be_nil
+      expect(user_count + 1 ).to eq(new_user_count)
+    end
+  end
 end
