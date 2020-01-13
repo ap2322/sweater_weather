6c34c6ad499a40a02d9808abb3d7e591158b8589
Alice Post <alicep2322@gmail.com>
2020-01-13 10:20:07 -0700

    Remove hardcoded amypode

--- app/services/amypode_service.rb
+++ app/services/amypode_service.rb
@@ -7,12 +7,12 @@ class AmypodeService
     response = Faraday.get('http://amypode.herokuapp.com/api/v1/antipodes') do |req|
       req.params['lat'] = lat
       req.params['long'] = long
-      req.headers['api_key'] = 'oscar_the_grouch'
+      req.headers['api_key'] = ENV['AMYPODE_API_KEY']
     end
     json_response = JSON.parse(response.body, symbolize_names: true)
     anti_lat = json_response[:data][:attributes][:lat]
     anti_long = json_response[:data][:attributes][:long]
     "#{anti_lat},#{anti_long}"
   end
-  
+
 end
