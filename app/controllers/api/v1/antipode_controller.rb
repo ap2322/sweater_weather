25587d7f76db13b15fd49807796b3dd165dfefd6
Alice Post <alicep2322@gmail.com>
2020-01-13 10:15:40 -0700

    Update controller with serialized antipode forecast

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -12,6 +12,13 @@ class Api::V1::AntipodeController < ApplicationController
     antipode_location_name = google_service.find_place(antipode_lat_long)
 
     # send antipode lat and long to darksky api for current weather summary and temp
+    darksky_service = DarkskyService.new(antipode_lat_long)
+    antipode_forecast = darksky_service.forecast
+
+    # Serialize antipode forecast
+    hash_output = AntipodeForecastSerializer.serializable_hash(
+      params[:location], antipode_location_name, antipode_forecast)
+    render json: hash_output.to_json
   end
 
 end
