6a455a3efc95f6ef68299746019c138a3c30b2c4
Alice Post <alicep2322@gmail.com>
2020-01-13 10:16:41 -0700

    Update test

--- spec/requests/api/v1/antipode_request_spec.rb
+++ spec/requests/api/v1/antipode_request_spec.rb
@@ -2,20 +2,18 @@ require 'rails_helper'
 
 describe 'Antipode weather lookup api' do
   it 'returns the city name and current weather of that city', :vcr do
-    WebMock.enable_net_connect!
-    VCR.eject_cassette
-    VCR.turn_off!(ignore_cassettes: true)
     city = 'Hong Kong'
     get "/api/v1/antipode?location=#{city}"
 
     expect(response).to be_successful
 
     parsed_response = JSON.parse(response.body, symbolize_names: true)
-    expect(parsed_response).to have_key :location_name
-    expect(parsed_response).to have_key :forecast
-    expect(parsed_response[:forecast]).to have_key :summary
-    expect(parsed_response[:forecast]).to have_key :current_temperature
-    expect(parsed_response).to have_key :search_location
+
+    expect(parsed_response[:attributes]).to have_key :location_name
+    expect(parsed_response[:attributes]).to have_key :forecast
+    expect(parsed_response[:attributes][:forecast]).to have_key :summary
+    expect(parsed_response[:attributes][:forecast]).to have_key :current_temperature
+    expect(parsed_response[:attributes]).to have_key :search_location
 
   end
 end
