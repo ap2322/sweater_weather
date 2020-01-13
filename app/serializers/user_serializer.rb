1a60d9b844f6c13443df6e8e2e53cc8c314ff9ae
Alice Post <alicep2322@gmail.com>
2020-01-12 22:50:18 -0700

    Remove unnecessary conditionals from serializable_hash override

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,16 +4,8 @@ class UserSerializer
 
   def serializable_hash
     data = super
-
     if data[:data].is_a? Hash
       data[:data][:attributes]
-
-    elsif data[:data].is_a? Array
-      data[:data].map{ |x| x[:attributes] }
-
-    elsif data[:data] == nil
-      nil
-
     else
       data
     end
