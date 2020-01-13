997a9efaee0edd0f0be29ceb1e155a72ae93fd81
Alice Post <alicep2322@gmail.com>
2020-01-13 11:08:02 -0700

    Refactor service into two methods

--- app/services/amypode_service.rb
+++ app/services/amypode_service.rb
@@ -1,18 +1,20 @@
 class AmypodeService
 
   def self.antipode(lat_long)
-    ll_array = lat_long.split(',')
-    lat = ll_array[0]
-    long = ll_array[1]
-    response = Faraday.get('http://amypode.herokuapp.com/api/v1/antipodes') do |req|
-      req.params['lat'] = lat
-      req.params['long'] = long
-      req.headers['api_key'] = ENV['AMYPODE_API_KEY']
-    end
-    json_response = JSON.parse(response.body, symbolize_names: true)
+    json_response = fetch_json_response(lat_long)
     anti_lat = json_response[:data][:attributes][:lat]
     anti_long = json_response[:data][:attributes][:long]
     "#{anti_lat},#{anti_long}"
   end
 
+  private
+
+  def self.fetch_json_response(lat_long)
+    response = Faraday.get('http://amypode.herokuapp.com/api/v1/antipodes') do |req|
+      req.params['lat'] = lat_long.split(',')[0]
+      req.params['long'] = lat_long.split(',')[1]
+      req.headers['api_key'] = ENV['AMYPODE_API_KEY']
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
