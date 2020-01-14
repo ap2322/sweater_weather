3d2069b777c8dea8e17adef657226b3e93e9f1a1
Alice Post <alicep2322@gmail.com>
2020-01-13 20:33:32 -0700

    Notes on how to make a request spec vs a controller one

--- spec/requests/api/v1/register_user_spec.rb
+++ spec/requests/api/v1/register_user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
+# RSpec.describe 'Register User API', :type => :request do
+#   describe 'register user endpoint' do
+#     it 'recieves user info and responds with an api key' do
+#       body = {
+#         "email": "whatever@example.com",
+#         "password": "password",
+#         "password_confirmation": "password",
+#       }
+#
+#       post '/api/v1/users', params: body
+#       api_key_response = JSON.parse(response.body)
+#       binding.pry
+#
+#       expect(api_key_response).to be_a Hash
+#       expect(api_key_response).to have_key 'api_key'
+#     end
+#   end
+# end
+
 RSpec.describe Api::V1::UsersController, :type => :controller do
   describe 'register user endpoint' do
     it 'recieves user info and responds with an api key' do
@@ -9,7 +28,8 @@ RSpec.describe Api::V1::UsersController, :type => :controller do
         "password_confirmation": "password",
       }
 
-      post :create, body: body.to_json
+      r = post :create, body: body.to_json
+      expect(r.request.env['PATH_INFO']).to eq '/api/v1/users'
       api_key_response = JSON.parse(response.body)
 
       expect(api_key_response).to be_a Hash
