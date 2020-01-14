760b4684972aa5253b235034a70b4c9b885aa245
Alice Post <alicep2322@gmail.com>
2020-01-14 14:05:51 -0700

    Fix test to use params instead of the controller test

--- spec/requests/api/v1/register_user_spec.rb
+++ spec/requests/api/v1/register_user_spec.rb
@@ -3,17 +3,15 @@ require 'rails_helper'
 RSpec.describe 'Register User API', :type => :request do
   describe 'register user endpoint' do
     it 'recieves user info and responds with an api key' do
+      User.destroy_all
       body = {
         "email": "whatever@example.com",
         "password": "password",
         "password_confirmation": "password",
       }
-      headers = {
-        'Content-Type' => 'application/json',
-        'Accept' => ' application/json'
-      }
 
-      post '/api/v1/users', params: {user: body}.to_json, headers: headers.to_json
+      post '/api/v1/users', params: {user: body}
+
       api_key_response = JSON.parse(response.body)
 
       expect(api_key_response).to be_a Hash
@@ -21,22 +19,3 @@ RSpec.describe 'Register User API', :type => :request do
     end
   end
 end
-
-# RSpec.describe Api::V1::UsersController, :type => :controller do
-#   describe 'register user endpoint' do
-#     it 'recieves user info and responds with an api key' do
-#       body = {
-#         "email": "whatever@example.com",
-#         "password": "password",
-#         "password_confirmation": "password",
-#       }
-#
-#       r = post :create, body: body.to_json
-#       expect(r.request.env['PATH_INFO']).to eq '/api/v1/users'
-#       api_key_response = JSON.parse(response.body)
-#
-#       expect(api_key_response).to be_a Hash
-#       expect(api_key_response).to have_key 'api_key'
-#     end
-#   end
-# end
