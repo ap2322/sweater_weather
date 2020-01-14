d9691c2cb2fecd74dd38a52b62f123b89399b4c4
Alice Post <alicep2322@gmail.com>
2020-01-13 20:42:33 -0700

    Clean up google geo service a bit

--- app/services/google_geo_service.rb
+++ app/services/google_geo_service.rb
@@ -1,36 +1,37 @@
 class GoogleGeoService
   def initialize(location)
     @location = location
-    @response ||= response
+    @response ||= get_response
   end
 
-  def coordnates
-    json_response = JSON.parse(@response.body, symbolize_names: true)
-    lat_long = json_response[:results][0][:geometry][:location].values.join(',')
+  def coordinates
+    json_info = json_response
+    json_info[:results][0][:geometry][:location].values.join(',')
   end
 
   def city_state_country
-    json_response = JSON.parse(@response.body, symbolize_names: true)
-    city = json_response[:results][0][:address_components][0][:long_name]
-    state = json_response[:results][0][:address_components][2][:short_name]
-    country = json_response[:results][0][:address_components][3][:long_name]
-    {location: "#{city}, #{state}", country: country}
+    location_name_hash(json_response)
   end
 
   private
-  attr_reader :location
+  attr_reader :location, :response
 
-  def conn
-    Faraday.new(
-      url: 'https://maps.googleapis.com/maps/api/',
-      params: {'key' => ENV['GOOGLE_GEO_API_KEY']}
-    )
+  def get_response
+    Faraday.get('https://maps.googleapis.com/maps/api/geocode/json') do |req|
+      req.params['address'] = location
+      req.params['key'] = ENV['GOOGLE_GEO_API_KEY']
+    end
   end
 
-  def response
-    conn.get('geocode/json') do |req|
-      req.params['address'] = location
-    end
+  def json_response
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def location_name_hash(json_info)
+    city = json_info[:results][0][:address_components][0][:long_name]
+    state = json_info[:results][0][:address_components][2][:short_name]
+    country = json_info[:results][0][:address_components][3][:long_name]
+    {location: "#{city}, #{state}", country: country}
   end
 
 end
