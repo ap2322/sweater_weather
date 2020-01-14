296c3a003cb964e176300d0e93b4cf2e9518fc84
Alice Post <alicep2322@gmail.com>
2020-01-14 11:47:32 -0700

    Move forecast methods to the forecast poro

--- spec/poros/weather_spec.rb
+++ spec/poros/weather_spec.rb
@@ -34,23 +34,4 @@ describe Weather do
 
     expect(@weather.details).to eq result_details
   end
-
-  xit 'makes an hourly and five day forecast hash', :vcr do
-    @weather = Weather.new('San Jose,CA')
-    result_five_day = {:hourly_data=>
-                        [{:time=>"10", :temperature=>48.4},
-                         {:time=>"11", :temperature=>46.67},
-                         {:time=>"12", :temperature=>47.3},
-                         {:time=>" 1", :temperature=>50.85},
-                         {:time=>" 2", :temperature=>51.26},
-                         {:time=>" 3", :temperature=>51.39}],
-                       :daily_data=>
-                        [{:day=>"Tuesday", :icon=>"rain", :precip_probability=>0.49, :precip_type=>"rain", :temp_high=>54.81, :temp_low=>39.03},
-                         {:day=>"Wednesday", :icon=>"cloudy", :precip_probability=>0.12, :precip_type=>"rain", :temp_high=>54.51, :temp_low=>38.2},
-                         {:day=>"Thursday", :icon=>"rain", :precip_probability=>0.95, :precip_type=>"rain", :temp_high=>51.29, :temp_low=>40.99},
-                         {:day=>"Friday", :icon=>"cloudy", :precip_probability=>0.16, :precip_type=>"rain", :temp_high=>51.96, :temp_low=>38.88},
-                         {:day=>"Saturday", :icon=>"cloudy", :precip_probability=>0.31, :precip_type=>"rain", :temp_high=>53.98, :temp_low=>41.63}]}
-
-    expect(@weather.five_day_forecast).to eq result_five_day
-  end
 end
