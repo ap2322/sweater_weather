827b80b716d198164984a5c89f7e9947b8f8998f
Alice Post <alicep2322@gmail.com>
2020-01-14 16:05:19 -0700

    Make specail api_key_response hash

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,8 +3,7 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: user_info[:email])
     if user && user.authenticate(user_info[:password])
-      api_key = { api_key: user.api_key }
-      render json: api_key, status: 200
+      render json: api_key_response(user), status: 200
     else
       render json: error_response, status: 400
     end
@@ -19,4 +18,7 @@ class Api::V1::SessionsController < ApplicationController
     { error: 'credentials are bad' }
   end
 
+  def api_key_response(user)
+    { api_key: user.api_key }
+  end
 end
