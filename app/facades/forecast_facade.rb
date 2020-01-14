81b2152d9f24ae7d3609142359f491b736b66e0e
Alice Post <alicep2322@gmail.com>
2020-01-14 11:46:13 -0700

    Make forecast_facade use forecast poro

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -3,16 +3,20 @@ class ForecastFacade
 
   def initialize(location)
     @location = location
+    @weather = Weather.new(location)
+    @forecast = Forecast.new(weather.lat_long)
   end
 
   def forecast_as_json
-    weather = Weather.new(location)
     all_details = {
       summary: weather.summary,
       details: weather.details,
-      five_day_forecast: weather.five_day_forecast,
+      five_day_forecast: forecast.five_day_and_hourly,
     }
     JSON.generate(all_details)
   end
+
+  private
+  attr_reader :weather, :forecast
 
 end
