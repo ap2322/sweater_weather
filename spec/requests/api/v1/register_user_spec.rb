1cb64fc4d38329eefdd8b3386294685912ab3a05
Alice Post <alicep2322@gmail.com>
2020-01-14 14:30:37 -0700

    Add sad paths for user registration spec

--- spec/requests/api/v1/register_user_spec.rb
+++ spec/requests/api/v1/register_user_spec.rb
@@ -16,6 +16,34 @@ RSpec.describe 'Register User API', :type => :request do
 
       expect(api_key_response).to be_a Hash
       expect(api_key_response).to have_key 'api_key'
+      expect(response.status).to eq 201
+    end
+
+    it 'does not make a user if missing information' do
+      User.destroy_all
+      body = {
+        "email": "whatever@example.com",
+        "password": "password",
+      }
+
+      post '/api/v1/users', params: {user: body}
+
+      error_response = JSON.parse(response.body)
+      expected_error = {"errors"=>["Password confirmation can't be blank"]}
+      expect(error_response).to eq expected_error
+      expect(response.status).to eq 400
+    end
+
+    it 'can return all error responses' do
+      new_body = {
+        "email": "whatever@example.com",
+      }
+      post '/api/v1/users', params: {user: new_body}
+
+      error_responses = JSON.parse(response.body)
+      expected_errors = {"errors"=>["Password can't be blank", "Password confirmation can't be blank"]}
+      expect(error_responses).to eq expected_errors
+      expect(response.status).to eq 400
     end
   end
 end
