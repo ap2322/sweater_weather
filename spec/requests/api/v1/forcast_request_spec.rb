0ef9cdc0760278ba49584e87c0ce92e38a46bf8b
Alice Post <alicep2322@gmail.com>
2020-01-14 11:48:49 -0700

    Re-record cassette for forecast api and remove special config

--- spec/requests/api/v1/forcast_request_spec.rb
+++ spec/requests/api/v1/forcast_request_spec.rb
@@ -2,16 +2,14 @@ require 'rails_helper'
 
 describe "Forecast API" do
   it "sends a json string of forecast information", :vcr do
-    VCR.use_cassette('sends_a_json_string_of_forecast_information.yml', :match_requests_on => [:method, :path]) do
       get '/api/v1/forecast?location=denver,co'
-    end
-    expect(response).to be_successful
+      expect(response).to be_successful
 
-    parsed_response = JSON.parse(response.body, symbolize_names: true)
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
 
-    expect(parsed_response).to have_key :summary
-    expect(parsed_response).to have_key :details
-    expect(parsed_response).to have_key :five_day_forecast
+      expect(parsed_response).to have_key :summary
+      expect(parsed_response).to have_key :details
+      expect(parsed_response).to have_key :five_day_forecast
 
   end
 end
