c8131c6935d85a8399725aa25cd4dacfff3ce0d4
Alice Post <alicep2322@gmail.com>
2020-01-14 14:30:25 -0700

    Add password and password_confirmation validations to user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ describe User, type: :model do
   describe 'Validations' do
     it {should validate_presence_of :email}
     it {should validate_presence_of :email}
+    it {should validate_presence_of :password_confirmation}
+    it {should validate_presence_of :password}
     it {should validate_uniqueness_of :email}
   end
 
