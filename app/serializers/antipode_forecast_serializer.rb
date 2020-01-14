18e1d8458a7ddff4941f381b6c7af4b252b44b69
Alice Post <alicep2322@gmail.com>
2020-01-14 13:20:53 -0700

    Refactor of to use fastjsonapi with attr readers of facade

--- app/serializers/antipode_forecast_serializer.rb
+++ app/serializers/antipode_forecast_serializer.rb
@@ -1,15 +1,24 @@
 class AntipodeForecastSerializer
-  def self.serializable_hash(search_location, antipode_location, full_forecast)
-    {
-      type: 'antipode',
-      attributes: {
-        location_name: antipode_location,
-        forecast: {
-          summary: full_forecast[:currently][:summary],
-          current_temperature: full_forecast[:currently][:temperature],
-        },
-        search_location: search_location
-      }
-    }
-  end
+  include FastJsonapi::ObjectSerializer
+  attributes :location_search, :antipode_location_name, :forecast
+  # def initialize(antipode_facade)
+  #   @search_location = antipode_facade.location_search
+  #   @location_name = antipode_facade.antipode_location_name
+  #   @forecast_summary = antipode_facade.antipode_forecast[:currently][:summary]
+  #   @forecast_temperature = antipode_facade.antipode_forecast[:currently][:temperature]
+  # end
+  #
+  # def serializable_hash(antipode)
+  #   {
+  #     type: 'antipode',
+  #     attributes: {
+  #       location_name: antipode_location,
+  #       forecast: {
+  #         summary: full_forecast[:currently][:summary],
+  #         current_temperature: full_forecast[:currently][:temperature],
+  #       },
+  #       search_location: search_location
+  #     }
+  #   }
+  # end
 end
