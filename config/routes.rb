04941e22e1cd354b5c5bc9904c3e897ea9f7dbf0
Alice Post <alicep2322@gmail.com>
2020-01-14 18:19:51 -0700

    Add route for backgrounds and rewrite non-resource routes as strings

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/forecast', to: 'forecast#index'
+      get '/backgrounds', to: 'backgrounds#index'
       resources :users, only: [:create], defaults: { format: :json }
-      resources :sessions, only: [:create], defaults: { format: :json }
+      post '/sessions', to: 'sessions#create'
     end
   end
 end
