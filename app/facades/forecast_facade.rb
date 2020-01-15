47fba6c9cb7c9ec5eaea5134ffe85925a38d7d4b
Alice Post <alicep2322@gmail.com>
2020-01-15 10:25:41 -0700

    Refactor forecast_facade to use objects for serailization and srp

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,21 +1,29 @@
 class ForecastFacade
-  attr_reader :location
+  attr_reader :location,
+              :summary,
+              :details,
+              :forecast,
+              :id
 
   def initialize(location)
     @location = location
     @weather = Weather.new(location)
-    @forecast = Forecast.new(weather.lat_long)
+    @forecast_aggregator = Forecast.new(weather.lat_long)
+    @summary = weather.summary
+    @details = weather.details
+    @forecast = forecast_aggregator.five_day_and_hourly
+    @id = 1
   end
 
-  def forecast_hash
-    {
-      summary: weather.summary,
-      details: weather.details,
-      five_day_forecast: forecast.five_day_and_hourly,
-    }
-  end
+  # def forecast_hash
+  #   {
+  #     summary: weather.summary,
+  #     details: weather.details,
+  #     five_day_forecast: forecast.five_day_and_hourly,
+  #   }
+  # end
 
   private
-  attr_reader :weather, :forecast
+  attr_reader :weather, :forecast_aggregator
 
 end
