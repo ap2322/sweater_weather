afe18e57be233900dd315b25977eb3be474ab966
Alice Post <alicep2322@gmail.com>
2020-01-14 20:19:35 -0700

    Add road trip request test with authorization check

---
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe 'Road Trip API Endpoint', type: :request do
+  xit 'returns origin, destination, travel time, and arrival forecast with a successful request', :vcr do
+    WebMock.enable_net_connect!
+    VCR.eject_cassette
+    VCR.turn_off!(ignore_cassettes: true)
+
+    user = User.create(email: 'road@tripper.com', password: 'something', password_confirmation: 'something')
+    body = {
+              "origin": "Denver,CO",
+              "destination": "Pueblo,CO",
+              "api_key": "#{user.api_key}"
+            }
+
+    post '/api/v1/road_trip', params: body
+    road_trip_info = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.status).to eq 200
+    expect(road_trip_info).to have_key :origin
+    expect(road_trip_info).to have_key :destination
+    expect(road_trip_info).to have_key :travel_time
+    expect(road_trip_info).to have_key :arrival_forecast
+    expect(road_trip_info).to have_key :temperature
+    expect(road_trip_info).to have_key :summary
+  end
+
+  it 'prevents access with wrong or no token' do
+    user = User.create(email: 'road@tripper.com', password: 'something', password_confirmation: 'something')
+    body = {
+              "origin": "Denver,CO",
+              "destination": "Pueblo,CO",
+              "api_key": "something_else_not_in_db"
+            }
+
+    post '/api/v1/road_trip', params: body
+
+    error_info = JSON.parse(response.body, symbolize_names: true)
+    expect(response.status).to eq 401
+    expect(error_info).to eq({error:"Unauthorized request"})
+  end
+end
