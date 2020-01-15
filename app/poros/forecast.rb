8221d8b7a51465ba90d08a5194915291213dcc46
Alice Post <alicep2322@gmail.com>
2020-01-15 11:56:53 -0700

    remove unused ivar time

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -1,9 +1,8 @@
 class Forecast
   attr_reader :hourly, :daily
 
-  def initialize(lat_long, time = Time.now, current_full_forecast)
+  def initialize(lat_long, current_full_forecast)
     @lat_long = lat_long
-    @time = time
     @hourly = make_hourly_forecast(current_full_forecast)
     @daily = get_five_day_forecast(current_full_forecast)
   end
