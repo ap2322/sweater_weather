b2525fd33170a506c1aef5e257b3dc18937de724
Alice Post <alicep2322@gmail.com>
2020-01-15 11:25:30 -0700

    Refactor forecast poro into daily and hourly poros instead of building hashes

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -1,51 +1,43 @@
 class Forecast
-  attr_reader :hourly, :five_days
+  attr_reader :hourly, :daily
 
-  def initialize(lat_long, time = Time.now)
+  def initialize(lat_long, time = Time.now, current_full_forecast)
     @lat_long = lat_long
     @time = time
-    @current_full_forecast = darksky_current_full_forecast(lat_long)
-    @hourly = get_hourly_forecast
-    @five_days = get_five_day_forecast
+    @hourly = make_hourly_forecast(current_full_forecast)
+    @daily = get_five_day_forecast(current_full_forecast)
   end
 
-  def get_hourly_forecast
-    next_hours = current_full_forecast[:hourly][:data][0..5]
-    next_hours.map do |hour_info|
-      {
-        time: Time.at(hour_info[:time]).strftime("%l %p"),
-        temperature: hour_info[:temperature]
-      }
-    end
+  def make_hourly_forecast(current_full_forecast)
+    # @_current_full_forecast ||= darksky_current_full_forecast(lat_long)
+    hours = 7
+    HourlyForecast.new(current_full_forecast, hours)
+    # next_hours = current_full_forecast[:hourly][:data][0..5]
+    # next_hours.map do |hour_info|
+    #   {
+    #     time: Time.at(hour_info[:time]).strftime("%l %p"),
+    #     temperature: hour_info[:temperature]
+    #   }
+    # end
   end
 
-  def get_five_day_forecast
-    next_five_days = current_full_forecast[:daily][:data][1..5]
-    next_five_days.map do |daily_info|
-      {
-        day: Time.at(daily_info[:time]).strftime("%A"),
-        icon: daily_info[:icon],
-        precip_probability: daily_info[:precipProbability],
-        precip_type: daily_info[:precipType],
-        temp_high: daily_info[:temperatureMax],
-        temp_low: daily_info[:temperatureMin],
-      }
-    end
-  end
-
-  def five_day_and_hourly
-    {
-      hourly_data: hourly,
-      daily_data: five_days,
-    }
+  def get_five_day_forecast(current_full_forecast)
+    days = 5
+    DailyForecast.new(current_full_forecast, days)
+    # next_five_days = current_full_forecast[:daily][:data][1..5]
+    # next_five_days.map do |daily_info|
+    #   {
+    #     day: Time.at(daily_info[:time]).strftime("%A"),
+    #     icon: daily_info[:icon],
+    #     precip_probability: daily_info[:precipProbability],
+    #     precip_type: daily_info[:precipType],
+    #     temp_high: daily_info[:temperatureMax],
+    #     temp_low: daily_info[:temperatureMin],
+    #   }
+    # end
   end
 
   private
-  attr_reader :lat_long, :time, :current_full_forecast
-
-  def darksky_current_full_forecast(lat_long)
-    @_darksky_service ||= DarkskyService.new(lat_long)
-    @_darksky_service.forecast
-  end
+  attr_reader :lat_long, :time
 
 end
