37b82dbdc9c2cabe96ae8016a3090414b94a3ddc
Alice Post <alicep2322@gmail.com>
2020-01-15 11:24:51 -0700

    Make api calls in forecast facade instead of poros

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -7,23 +7,57 @@ class ForecastFacade
 
   def initialize(location)
     @location = location
-    @weather = Weather.new(location)
-    @forecast_aggregator = Forecast.new(weather.lat_long)
+    @location_info = google_location_info
+    @lat_long = google_lat_long
+    @current_forecast = darksky_current_full_forecast(lat_long)
+    @evening_forecast = darksky_evening_forecast(lat_long)
+    @weather = make_weather
+    @forecast = make_forecast
     @summary = weather.summary
     @details = weather.details
-    @forecast = forecast_aggregator.five_day_and_hourly
     @id = 1
   end
 
-  # def forecast_hash
-  #   {
-  #     summary: weather.summary,
-  #     details: weather.details,
-  #     five_day_forecast: forecast.five_day_and_hourly,
-  #   }
-  # end
+  def make_weather
+    Weather.new(location, lat_long, location_info, current_forecast, evening_forecast)
+  end
+
+  def make_forecast
+    Forecast.new(weather.lat_long, current_forecast)
+  end
 
   private
-  attr_reader :weather, :forecast_aggregator
+  attr_reader :weather,
+              :forecast_aggregator,
+              :lat_long,
+              :location_info,
+              :current_forecast,
+              :evening_forecast
 
+  def location_name_hash(json_info)
+    city = json_info[:results][0][:address_components][0][:long_name]
+    state = json_info[:results][0][:address_components][2][:short_name]
+    country = json_info[:results][0][:address_components][3][:long_name]
+    {location: "#{city}, #{state}", country: country}
+  end
+
+  def google_location_info
+    google_service = GoogleGeoService.new(location)
+    @_geo_info ||= google_service.get_geographic_info
+    location_name_hash(@_geo_info)
+  end
+
+  def google_lat_long
+    @_geo_info[:results][0][:geometry][:location].values.join(',')
+  end
+
+  def darksky_current_full_forecast(lat_long)
+    @_darksky_service ||= DarkskyService.new(lat_long)
+    current_full_forecast = @_darksky_service.forecast
+  end
+
+  def darksky_evening_forecast(lat_long)
+    @_darksky_service ||= DarkskyService.new(lat_long)
+    evening_forecast = @_darksky_service.specific_forecast
+  end
 end
