fd43edde249885e3e46dd9d2a84080b46106d9e4
Alice Post <alicep2322@gmail.com>
2020-01-15 11:35:10 -0700

    Update test for full expected response structure of hash

--- spec/requests/api/v1/forcast_request_spec.rb
+++ spec/requests/api/v1/forcast_request_spec.rb
@@ -2,14 +2,48 @@ require 'rails_helper'
 
 describe "Forecast API" do
   it "sends a json string of forecast information", :vcr do
-      get '/api/v1/forecast?location=denver,co'
-      expect(response).to be_successful
+    get '/api/v1/forecast?location=denver,co'
+    expect(response).to be_successful
 
-      parsed_response = JSON.parse(response.body, symbolize_names: true)
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+    attributes = parsed_response[:data][:attributes]
 
-      expect(parsed_response).to have_key :summary
-      expect(parsed_response).to have_key :details
-      expect(parsed_response).to have_key :five_day_forecast
+    expect(attributes).to have_key :summary
+    expect(attributes[:summary]).to have_key :location
+    expect(attributes[:summary]).to have_key :country
+    expect(attributes[:summary]).to have_key :time
+    expect(attributes[:summary]).to have_key :date
+    expect(attributes[:summary]).to have_key :temperature
+    expect(attributes[:summary]).to have_key :temperature_high
+    expect(attributes[:summary]).to have_key :temperature_low
+    expect(attributes[:summary]).to have_key :summary
+    expect(attributes[:summary]).to have_key :icon
+
+    expect(attributes).to have_key :details
+    expect(attributes[:details]).to have_key :icon
+    expect(attributes[:details]).to have_key :summary
+    expect(attributes[:details]).to have_key :today_details
+    expect(attributes[:details]).to have_key :tonight_details
+    expect(attributes[:details]).to have_key :feels_like
+    expect(attributes[:details]).to have_key :humidity
+    expect(attributes[:details]).to have_key :visibility_miles
+    expect(attributes[:details]).to have_key :uv_index
+    expect(attributes[:details]).to have_key :uv_index_relative
+
+    expect(attributes).to have_key :forecast
+    expect(attributes[:forecast]).to have_key :hourly
+    expect(attributes[:forecast][:hourly]).to have_key :hourly_data
+    expect(attributes[:forecast][:hourly][:hourly_data][0]).to have_key :time
+    expect(attributes[:forecast][:hourly][:hourly_data][0]).to have_key :temperature
+
+    expect(attributes[:forecast]).to have_key :daily
+    expect(attributes[:forecast][:daily]).to have_key :daily_data
+    expect(attributes[:forecast][:daily][:daily_data][0]).to have_key :day
+    expect(attributes[:forecast][:daily][:daily_data][0]).to have_key :icon
+    expect(attributes[:forecast][:daily][:daily_data][0]).to have_key :precip_probability
+    expect(attributes[:forecast][:daily][:daily_data][0]).to have_key :precip_type
+    expect(attributes[:forecast][:daily][:daily_data][0]).to have_key :temp_high
+    expect(attributes[:forecast][:daily][:daily_data][0]).to have_key :temp_low
 
   end
 end
