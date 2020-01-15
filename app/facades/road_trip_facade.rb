6aa5755fbc5183dc73d6f55a3ed094582b2ac1aa
Alice Post <alicep2322@gmail.com>
2020-01-14 21:04:18 -0700

    Finish out road trip facade with travel time and arrival forecast methods

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -2,17 +2,37 @@ class RoadTripFacade
   attr_reader :origin,
               :destination,
               :travel_time,
-              :arrival_forecast
+              :arrival_forecast,
+              :id
 
   def initialize(info)
     @origin = info[:origin]
     @destination = info[:destination]
     @travel_time = fetch_travel_time
-    @arrival_forecast = get_arrival_forecast
+    @arrival_forecast = fetch_arrival_forecast
+    @id = 1
   end
 
   def fetch_travel_time
-    # google directions service call and parse for travel time
+    service = GoogleDirectionsService.new(origin, destination)
+    @_directions_info ||= service.directions
+    @_directions_info[:routes][0][:legs][0][:duration][:text]
+  end
+
+  def fetch_arrival_forecast
+    service = DarkskyService.new(destination_lat_long)
+    forecast_info = service.specific_forecast(destination_lat_long, destination_time, destination_time.hour)
+    RoadTripForecast.new(forecast_info[:currently])
+  end
+
+  private
+
+  def destination_lat_long
+    @_directions_info[:routes][0][:legs][0][:end_location].values.join(',')
+  end
+
+  def destination_time
+    Time.now + @_directions_info[:routes][0][:legs][0][:duration][:value]
   end
 
 
