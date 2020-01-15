968b89093f438a881e0a1cd40d47c6fc72349a90
Alice Post <alicep2322@gmail.com>
2020-01-15 15:13:59 -0700

    Notes on abstraction

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -8,31 +8,33 @@ class RoadTripFacade
   def initialize(info)
     @origin = info[:origin]
     @destination = info[:destination]
-    @travel_time = fetch_travel_time
-    @arrival_forecast = fetch_arrival_forecast
+    @travel_time = make_travel_time
+    @arrival_forecast = fetch_arrival_forecast(@travel_time)
     @id = 1
   end
 
-  def fetch_travel_time
+  def make_travel_time
     service = GoogleDirectionsService.new(origin, destination)
     @_directions_info ||= service.directions
-    @_directions_info[:routes][0][:legs][0][:duration][:text]
+    # @_directions_info[:routes][0][:legs][0][:duration][:text]
+    TravelTime.new(@_directions_info)
   end
 
-  def fetch_arrival_forecast
-    service = DarkskyService.new(destination_lat_long)
-    forecast_info = service.specific_forecast(destination_lat_long, destination_time, destination_time.hour)
+  def fetch_arrival_forecast(travel_time)
+    service = DarkskyService.new(travel_time.lat_long)
+    forecast_info = service.specific_forecast(destination_lat_long, travel_time.destination_time, destination_time.hour)
     RoadTripForecast.new(forecast_info[:currently])
   end
 
   private
 
-  def destination_lat_long
-    @_directions_info[:routes][0][:legs][0][:end_location].values.join(',')
-  end
-
-  def destination_time
-    Time.now + @_directions_info[:routes][0][:legs][0][:duration][:value]
-  end
+  #
+  # def destination_lat_long
+  #   @_directions_info[:routes][0][:legs][0][:end_location].values.join(',')
+  # end
+  #
+  # def destination_time
+  #   Time.now + @_directions_info[:routes][0][:legs][0][:duration][:value]
+  # end
 
 end
